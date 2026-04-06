8e0caea841f4141b9cafc625474ab1836e5fa127
kipkirui-eng <kipkiruim89@gmail.com>
2026-04-06 04:05:56 -0400

    Switch to sandbox API

--- payments/views.py
+++ payments/views.py
@@ -110,8 +110,7 @@ def result(request):
 def stk_push(phone, amount):
     access_token = get_access_token()
 
-    url = "https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
-
+    url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
     shortcode = "174379" 
     passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
 
@@ -152,7 +151,7 @@ def get_access_token():
     consumer_key = "uAXvc84UhbEwlI89sMA6vX8yPFGzdLs6B3Gyn9WdsZm5wfh4"
     consumer_secret = "t5zCG1VT3EMWF87hjA0PiP1gNoZupF75hyAagHe1vBIk9JOFHhzUBkZAUJWT0Bpk"
 
-    url = "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 
     response = requests.get(url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
 
